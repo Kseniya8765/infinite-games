8ff780e8fbf0abb12693479311cdbf31b8a456f2
Kseniya8765 <tomashevich.kseniya@bk.ru>
2022-06-29 10:28:50 +0300

    add slider and accordion

---
+++ resources/scripts/slider.js
@@ -0,0 +1,24 @@
+import Swiper , { EffectFade } from 'swiper';
+
+export default class Slider {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    const sections = document.querySelectorAll('.slider');
+    sections.forEach((s) => {
+      const pagination = [...s.querySelectorAll('.slider-pagination__title')];
+      const mySwiper = new Swiper(s.querySelector('.slider-text'), {
+       modules: [ EffectFade] ,
+       effect: 'fade',
+      });
+      pagination.forEach((p, index) => {
+        p.addEventListener('mouseover', function (e) {
+          e.preventDefault();
+          mySwiper.slideTo(index);
+        });
+      });
+    });
+  }
+}
