89cfd54181c23ef9cc08c50fda6794897ad8ffc1
Kseniya8765 <tomashevich.kseniya@bk.ru>
2022-07-05 11:03:26 +0300

    fix slider and citation

--- resources/scripts/slider.js
+++ resources/scripts/slider.js
@@ -1,26 +1,47 @@
-import Swiper , { EffectFade } from 'swiper';
+import Swiper, { EffectFade, Pagination } from 'swiper';
 
 export default class Slider {
   constructor() {
+    this.sections = document.querySelectorAll('.slider');
+
     this.init();
   }
 
+  sliderInit(s) {
+    const menu = ['Technology', 'Production', 'Marketing', 'Advisory'];
+
+    const mySwiper = new Swiper(s.querySelector('.slider-text'), {
+      modules: [EffectFade, Pagination],
+      pagination: {
+        el: '.slider-pagination',
+        clickable: true,
+        renderBullet: function (index, className) {
+          return (
+            '<h2 class="' +
+            className +
+            ' slider-pagination__title">' +
+            menu[index] +
+            ' </h2>'
+          );
+        },
+      },
+      effect: 'fade',
+    });
+
+    const pagination = s.querySelectorAll('.swiper-pagination-bullet');
+
+    pagination.forEach((p, index) => {
+      p.addEventListener('mouseover', () => {
+        mySwiper.slideTo(index);
+      });
+    });
+  }
+
   init() {
-    const sections = document.querySelectorAll('.slider');
-    sections.forEach((s) => {
-      const pagination = [...s.querySelectorAll('.slider-pagination__title')];
-     
-      const mySwiper = new Swiper(s.querySelector('.slider-text'), {
-       modules: [ EffectFade] ,
-       effect: 'fade',
-      });
-      
-      pagination.forEach((p, index) => {
-        p.addEventListener('mouseover', function (e) {
-          e.preventDefault();
-          mySwiper.slideTo(index);
-        });
-      });
+    if (!this.sections) return;
+
+    this.sections.forEach((s) => {
+      this.sliderInit(s);
     });
   }
 }
